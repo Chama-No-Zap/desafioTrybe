291a541a2c230f3b9636c1f6375a593da92017d0
Henrique Rezende <henriquerezendec@gmail.com>
2020-08-16 13:00:33 -0300

    CSS

--- front-end/src/pages/LandingPage/index.js
+++ front-end/src/pages/LandingPage/index.js
@@ -1,45 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import wpp from '../../assets/images/icons/whatsapp.svg'
-import './styles.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import wpp from "../../assets/images/icons/whatsapp.svg";
+import "./styles.css";
 
-const linksBtn = (clasName, link, text) => {
-  return(
-    <p className={clasName}>
+const linksBtn = (className, link, text) => {
+  return (
+    <div className={className}>
       <Link to={`/${link}`}>{text}</Link>
-    </p>
+    </div>
   );
 };
 
 const LandingPage = () => {
   return (
     <div>
-      <h1 className="logo-name">AmbulaBev</h1>
-      {linksBtn('find-drinks-btn', 'FindDrinks', 'Encontrar Bebidas')}
+      <h1 className="logo-name">AMBULABEV</h1>
+      {linksBtn("find-drinks-btn", "FindDrinks", "Encontrar Bebidas")}
       <div className="infos">
-        <div>{linksBtn('partner-link', 'BecomePartner', 'Seja parceiro')}</div>
+        <div>{linksBtn("partner-link", "BecomePartner", "Seja parceiro")}</div>
         <div>
-        Links úteis
-        {linksBtn('sub-links', 'Devs', 'Quem somos')}
-        {linksBtn('sub-links', 'Feedbacks', 'Sugestões e feedbacks')}
+          Links úteis
+          {linksBtn("sub-links", "Devs", "Quem somos")}
+          {linksBtn("sub-links", "Feedbacks", "Sugestões e feedbacks")}
         </div>
         <div>
-        Ajuda
-        {linksBtn('sub-links', 'FindSellerHelp', 'Como encontar um vendedor?')}
-        {linksBtn('sub-links', 'RequirementsHelp', 'Quais requisitos pra ser um parceiro?')}
+          Ajuda
+          {linksBtn(
+            "sub-links",
+            "FindSellerHelp",
+            "Como encontar um vendedor?"
+          )}
+          {linksBtn(
+            "sub-links",
+            "RequirementsHelp",
+            "Quais requisitos pra ser um parceiro?"
+          )}
         </div>
-        <div>
-        Contato</div>
+        <div>Contato</div>
         <div className="contact">
-          <a rel="noopener noreferrer" target="_blank" href={`https://wa.me/973481195`}>
-            <img src={wpp} alt="whatsapp icon"/>
-            Entrar em contato
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={`https://wa.me/973481195`}
+          >
+            (31) 97348-1195
           </a>
         </div>
-        <div>email@contato.com</div>
+        <div className="sub-links">
+          <a href="mailto:email@contato.com">email@contato.com</a>
+        </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default LandingPage;
