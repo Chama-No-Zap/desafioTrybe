afd72901048844a0c8d7ff655f919f2ee768c243
Henrique Rezende <henriquerezendec@gmail.com>
2020-08-26 10:59:55 -0300

    Fixing filter

--- front-end/src/fakeUsersdatabase/index.js
+++ front-end/src/fakeUsersdatabase/index.js
@@ -4,7 +4,7 @@ const partners = [
     name: "Breno Mesquita",
     profilePicture:
       "https://instagram.fplu9-1.fna.fbcdn.net/v/t51.2885-19/s150x150/71709120_1506189656189198_5308546772895793152_n.jpg?_nc_ht=instagram.fplu9-1.fna.fbcdn.net&_nc_ohc=upGpNltVrnkAX8XhqzJ&oh=98caadc3b3801a0bc1e29fe5ba589dc7&oe=5F61F38F",
-    avaliableProducts: ["Água", "Refrigerante"],
+      avaliableProducts: [{name: "Água", price: "0.79"}, {name: "Refrigerante", price: "1.49"}, {name: "Suco", price: "1.69"}],
     localization: {
       lat: -19.930693,
       lng: -43.934909,
@@ -19,7 +19,7 @@ const partners = [
     id: 2,
     name: "Gustavo",
     profilePicture: null,
-    avaliableProducts: ["Água", "Refrigerante", "Suco"],
+    avaliableProducts: [{name: "Água", price: "0.69"}],
     localization: {
       lat: -19.932977,
       lng: -43.937686,
@@ -34,7 +34,7 @@ const partners = [
     id: 3,
     name: "Joaquim",
     profilePicture: null,
-    avaliableProducts: ["Água"],
+    avaliableProducts: [{name: "Água", price: "0.79"}, {name: "Refrigerante", price: "1.39"}],
     localization: {
       lat: -19.934443,
       lng: -43.934534,
@@ -49,7 +49,7 @@ const partners = [
     id: 4,
     name: "Marco",
     profilePicture: null,
-    avaliableProducts: ["Água", "Refrigerante"],
+    avaliableProducts: [{name: "Refrigerante", price: "1.69"}, {name: "Suco", price: "1.99"}],
     localization: {
       lat: -19.939882,
       lng: -43.934737,
@@ -64,7 +64,7 @@ const partners = [
     id: 5,
     name: "Ana",
     profilePicture: null,
-    avaliableProducts: ["Suco", "Refrigerante"],
+    avaliableProducts: [{name: "Água", price: "0.69"}, {name: "Suco", price: "1.79"}],
     localization: {
       lat: -19.933639,
       lng: -43.932935,
